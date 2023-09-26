8615df7d2ef138757892893f16ef9c6fe2b0cd5e
Rodney Leviton <rod@rodleviton.com>
2023-09-27 09:57:27 +1000

    Update next.config.js

--- packages/marketing/next.config.js
+++ packages/marketing/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true },
-  experimental: { images: { unoptimized: true } },
-  basePath: "/recast",
+  images: { unoptimized: isProduction },
+  basePath: isProduction ? "/recast" : "",
+  assetPrefix: isProduction ? "/recast" : "",
 };
 
 module.exports = nextConfig;
