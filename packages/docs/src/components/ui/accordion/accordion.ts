875847e9cfdab26d1bc3c35536138e05fa8bbee3
Rodney Leviton <rod@rodleviton.com>
2023-11-10 10:12:46 +1100

    microbundle

--- packages/docs/src/components/ui/accordion/accordion.ts
+++ packages/docs/src/components/ui/accordion/accordion.ts
@@ -1,5 +1,7 @@
 "use client";
 
-import { RecastAccordionPrimitive } from "@rpxl/recast-primitive";
+import { RecastAccordionPrimitive } from "@rpxl/recast-primitives/accordion";
+import { RecastAlertPrimitive } from "@rpxl/recast-primitives/alert";
+import { RecastAlertPrimitive as YO } from "@rpxl/recast-primitives";
 
 export const Accordion = RecastAccordionPrimitive.recast("Accordion", {});
