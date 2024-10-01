56e73ece80141997238e4894a0f1a0a350f66e14
Rodney Leviton <rod@rodleviton.com>
2024-10-01 21:33:29 +1000

    Remove new theme

--- packages/theme/components/ui/button/button.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { recast } from "@rpxl/recast";
-import { ButtonPrimitive } from "@rpxl/recast-primitives";
-
-export const Button = recast(ButtonPrimitive, {
-  base: "",
-});
--- packages/theme/components/ui/card/card-content.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { recast } from "@rpxl/recast";
-import { CardContentPrimitive } from "@rpxl/recast-primitives";
-
-export const CardContent = recast(CardContentPrimitive, {
-  base: ["py-12", "px-8"],
-});
