3a9b55f159d9c01f08df5845176cf3ab88e07a7c
Rodney Leviton <rod@arbiio.com.au>
2025-03-15 10:12:29 +1100

    Update page.tsx

--- packages/sandbox/app/page.tsx
+++ packages/sandbox/app/page.tsx
@@ -5,9 +5,20 @@ export default function Page() {
     <div className="p-8 flex flex-col gap-12 justify-center items-center w-full min-h-screen">
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
 
-      <Button size="sm" variant="secondary">
-        Sandbox
-      </Button>
+      <div className="flex flex-col gap-4 items-center">
+        <h2 className="text-2xl font-bold">Buttons</h2>
+        <div className="flex gap-4">
+          <Button size="sm" variant="primary">
+            Primary
+          </Button>
+          <Button size="sm" variant="secondary">
+            Secondary
+          </Button>
+          <Button size="sm" variant="tertiary">
+            Tertiary
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
