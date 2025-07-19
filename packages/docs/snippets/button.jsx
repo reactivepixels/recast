15eeb2d0862752984bfb3fd491da756115f62881
Rodney Leviton <rod@rodleviton.com>
2025-07-19 14:41:05 +1000

    Simplify docs

---
+++ packages/docs/snippets/button.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { recast } from "@rpxl/recast";
+
+export const Button = (({ children, ...props }, ref) => {
+  return (
+    <button ref={ref} {...props}>
+      {children}
+    </button>
+  );
+});
+
+// const buttonStyles = recast.styles({
+//   base: "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+//   variants: {
+//     variant: {
+//       default:
+//         "bg-black text-white hover:bg-black/90 dark:bg-white dark:text-black dark:hover:bg-white/90",
+//       destructive:
+//         "bg-red-500 text-white hover:bg-red-500/90 dark:bg-red-500 dark:hover:bg-red-500/90",
+//       outline:
+//         "border border-slate-200 bg-white hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
+//       secondary:
+//         "bg-slate-100 text-slate-900 hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
+//       ghost:
+//         "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
+//       link: "text-black underline-offset-4 hover:underline dark:text-white",
+//     },
+//     size: {
+//       default: "h-10 px-4 py-2",
+//       sm: "h-9 rounded-md px-3",
+//       lg: "h-11 rounded-md px-8",
+//       icon: "h-10 w-10",
+//     },
+//   },
+// });
+
+// export const Button = buttonStyles(ButtonPrimitive);
